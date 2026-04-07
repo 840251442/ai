ae411ebb7da7048978192fa160c877bd50da27fc
jiali <jiali@hecom.cn>
2026-04-07 14:25:49 +0800

    fix: 测试review机器人

--- twoSums.ts
+++ twoSums.ts
@@ -1,10 +1,4 @@
-/**
- * Finds two numbers in the array that add up to the target value.
- * @param nums - An array of numbers.
- * @param target - The target sum.
- * @returns A tuple containing the indices of the two numbers that add up to the target, or an empty array if no such pair exists.
- */
-function twoSum(nums: number[], target: number): [number, number] | [] {
+function a(nums: number[], target: number): [number, number] | [] {
     const indexMap = new Map<number, number>();
 
     for (let i = 0; i < nums.length; i += 1) {
@@ -18,4 +12,4 @@ function twoSum(nums: number[], target: number): [number, number] | [] {
     return [];
 }
 
-export default twoSum;
+export default a;
